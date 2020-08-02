6881f2f3a19e01f4fdeffe0128faea9aa75ef568
Big G <marc.gallardus@gmail.com>
2020-08-03 01:18:54 +0200

    M.G: Model creation and install some pods

---
+++ PunkeBeerApp/Pods/Target Support Files/Pods-PunkeBeerApp/Pods-PunkeBeerApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PunkeBeerApp : NSObject
+@end
+@implementation PodsDummy_Pods_PunkeBeerApp
+@end
---
+++ PunkeBeerApp/PunkeBeerApp/Main/Model/Beer.swift
@@ -0,0 +1,26 @@
+//
+//  Beer.swift
+//  PunkeBeerApp
+//
+//  Created by Marc Gallardo on 03/08/2020.
+//  Copyright © 2020 Marc Gallardo. All rights reserved.
+//
+
+import Foundation
+
+struct Beer: Codable {
+    let id: Int
+    let name: String
+    let tagline: String
+    let first_brewed: String
+    let description: String
+    let image_url: String
+    let abv: Double
+    let ibu: Double
+    let target_fg: Double
+    let target_og: Double
+    let ebc: Double
+    let srm: Double
+    let ph: Double
+    let attenuation_level: Double
+}
