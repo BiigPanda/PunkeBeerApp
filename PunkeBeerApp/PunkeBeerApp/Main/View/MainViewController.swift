73f6b7d9b4c0d4216fedf3c13ec30d2d02f4eae3
Big G <marc.gallardus@gmail.com>
2020-08-03 14:08:49 +0200

    Create Main Table View Cell

--- PunkeBeerApp/PunkeBeerApp/Main/View/MainViewController.swift
+++ PunkeBeerApp/PunkeBeerApp/Main/View/MainViewController.swift
@@ -9,22 +9,40 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableViewMain: UITableView!
+    
+    var viewModel = MainViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureView() {
+        viewModel.retriveDataList()
     }
-    */
+    
+    private func bind() {
+        viewModel.refreshData = { [weak self] () in
+            DispatchQueue.main.async {
+                self?.tableViewMain.reloadData()
+            }
+        }
+        
+    }
+}
 
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableViewMain.dequeueReusableCell(withIdentifier: "maintableviewcell") as! MainTableViewCell
+        return cell
+    }
+    
+    
+    
 }
